07c845cbb26d1d76eeec3db7ba011306fcec39e3
dorkele <dora_tominic@hotmail.com>
2021-02-15 11:32:56 +0100

    first commit

---
+++ src/components/Headline.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Headline() {
+    return (
+        <>
+            <h1>Latana Knowledge Center</h1>
+            <div>
+                Everything you need to know about the best brand tracking
+                solution in the world.
+            </div>
+        </>
+    );
+}
