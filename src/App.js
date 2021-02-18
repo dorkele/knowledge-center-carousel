5e1f09a5f83eb126bded59346c900763ccc4106a
dorkele <dora_tominic@hotmail.com>
2021-02-18 17:53:58 +0100

    Added resizing functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ import "./app.css";
 
 function App() {
     const [cardsShown, setCardsShown] = useState(3);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    //check for the size of the window
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWindowWidth(window.innerWidth);
+        });
+    }, []);
+
+    //number of the cards shown on the screen will depend on the size of the screen
+    useEffect(() => {
+        if (windowWidth < 768) {
+            setCardsShown(1);
+        } else if (720 < windowWidth && windowWidth < 1150) {
+            setCardsShown(2);
+        } else {
+            setCardsShown(3);
+        }
+    }, [windowWidth]);
 
     return (
         <div className="page">
