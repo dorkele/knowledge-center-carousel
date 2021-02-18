ce3ee2e7357d5fa368fa3674a3ab79783bff2fa8
dorkele <dora_tominic@hotmail.com>
2021-02-18 17:45:13 +0100

    Added buttons

---
+++ src/components/ArrowButtons/ArrowButtons.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./arrow-buttons.css";
+
+export default function ArrowButtons() {
+    return (
+        <div className="arrows">
+            <button className="left-arrow" />
+            <button className="right-arrow" />
+        </div>
+    );
+}
